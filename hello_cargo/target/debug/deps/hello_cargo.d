16e2ba7b7ed600a3d1065d564a4e5e31d9743050
JOHNNYBITMACHINE <johnnybitaso@outlook.com>
2025-06-24 00:32:08 +0530

    rust

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+K:\Program on Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+K:\Program on Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+K:\Program\ on\ Rust\hello_cargo\target\debug\hello_cargo.exe: K:\Program\ on\ Rust\hello_cargo\src\main.rs
